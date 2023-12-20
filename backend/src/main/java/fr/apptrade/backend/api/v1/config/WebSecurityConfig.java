ef4670ef4ad1e1ddfc95e2e72b9849bf1f8906e2
Thibault <zaouanethibault@icloud.com>
2023-12-20 14:06:16 +0100

    CORS & CSRF config

--- backend/src/main/java/fr/apptrade/backend/api/v1/config/WebSecurityConfig.java
+++ backend/src/main/java/fr/apptrade/backend/api/v1/config/WebSecurityConfig.java
@@ -7,11 +7,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 
 @Configuration
@@ -28,6 +34,7 @@ public class WebSecurityConfig {
 
     /**
      * Définit le type d'encodage des mots de passe
+     *
      * @return BCryptPasswordEncoder
      */
     @Bean
@@ -37,6 +44,9 @@ public class WebSecurityConfig {
 
     /**
      * Définit la méthode d'autentification et les urls autorisées
+     * Le CORS est activé pour toutes les urls
+     * Le CSRF est désactivé
+     *
      * @param http HttpSecurity
      * @return SecurityFilterChain
      * @throws Exception Exception
@@ -44,6 +54,8 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((requests) -> requests
                         .anyRequest().authenticated()
                 )
@@ -56,4 +68,19 @@ public class WebSecurityConfig {
         return http.build();
     }
 
+    /**
+     * Définit les paramètres du CORS
+     *
+     * @return CorsConfigurationSource
+     */
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
